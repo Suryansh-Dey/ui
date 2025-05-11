5444f4929161ccd59b062e3c50bcb6ee2a210e52
siduck <siduck@tutanota.com>
2025-05-11 14:22:25 +0530

    fix(nvdash): nostartup if curbuf is modifed #479

--- lua/nvchad/au.lua
+++ lua/nvchad/au.lua
@@ -3,10 +3,11 @@ local config = require "nvconfig"
 
 -- load nvdash only on empty file
 if config.nvdash.load_on_startup then
-  local opening_file = vim.fn.expand "%:p"
+  local opening_file = vim.api.nvim_buf_get_name(0)
   local is_dir = vim.fn.isdirectory(opening_file) == 1
+  local bufmodifed = vim.api.nvim_get_option_value("modified", { buf = 1 })
 
-  if is_dir or opening_file == "" then
+  if not bufmodifed and (is_dir or opening_file == "") then
     local current_buffer = vim.api.nvim_get_current_buf()
     require("nvchad.nvdash").open()
     vim.api.nvim_buf_delete(current_buffer, { force = true, unload = false })
